ee9c86f91ea72534ce2d6a2069c0f668f72f0d98
aarthiganesan2805 <aarthiganesan2805@gmail.com>
2026-06-02 12:40:36 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,5 +1,5 @@
 public class demo{
   public static void main(String args[]){
-    System.out.println("helloooo aarthiiii jenkinssssss");
+    System.out.println("helloooo aarthiii project");
   }
 }
